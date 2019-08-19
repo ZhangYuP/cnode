22142b818e27a8597ab158f4b9cb9cf3fb9806a8
ZhangYuPeng <zyp5318@qq.com>
2019-08-19 18:15:54 +0800

    Header 组件初步完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-
+    <Header></Header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Header from './components/Header'
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
 </script>
 
 <style>
-
+  * { margin: 0; padding: 0; box-sizing: border-box;}
+  ul, ol, li { list-style: none; }
 </style>
